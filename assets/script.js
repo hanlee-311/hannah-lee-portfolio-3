4b5698f4d8fc9be5551c7291f136326f79d60c4f
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-28 02:01:45 -0500

    added contact information in navbar, added tooltips to contacts, contacts are linked to proper websites

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
-  });
+});
 
-  
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
+});  
