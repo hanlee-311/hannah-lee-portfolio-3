677442559e1db06c08631a48601b090e8ea4813d
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-29 12:19:59 -0500

    hovering over projects will remove the title for the user to see the picture fully

--- assets/script.js
+++ assets/script.js
@@ -22,16 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems);
 });
 
-$(document).ready(function() {
-  $(".past-work").each(function() {
-    $(this)
-      .mouseover(function() {
-        // $(".text-wrapper").addClass('invisible');
-        $(".text-wrapper-2").addClass('invisible');
-      })
-      .mouseleave(function() {
-        // $(".text-wrapper").removeClass('invisible');
-        $(".text-wrapper-2").removeClass('invisible');
-      });
-  })
-});
+// $(document).ready(function() {
+//   $(".past-work").each(function() {
+//     $(this)
+//       .mouseover(function() {
+//         $("#one").addClass('invisible');
+//       })
+//       .mouseleave(function() {
+//         $("#one").removeClass('invisible');
+//       });
+//   })
+// });
