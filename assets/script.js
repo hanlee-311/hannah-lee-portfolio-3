e0e2ecffaa61fbb53cef783bf94555c28b11e57b
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-28 19:17:07 -0500

    put titles of work in center of images, began work on hover effects for this text

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,17 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
 });
+
+$(document).ready(function() {
+  $(".past-work").each(function() {
+    $(this)
+      .mouseover(function() {
+        // $(".text-wrapper").addClass('invisible');
+        $(".text-wrapper-2").addClass('invisible');
+      })
+      .mouseleave(function() {
+        // $(".text-wrapper").removeClass('invisible');
+        $(".text-wrapper-2").removeClass('invisible');
+      });
+  })
+});
